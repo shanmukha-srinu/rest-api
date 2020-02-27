0fab1783389d858177c694ead827ffb783e86de3
shanmukha srinivas <shanmukasrinivas010@gmail.com>
2020-02-27 21:38:08 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -53,7 +53,43 @@ app.get('/users/:id',async (req,res)=>{
     //     res.status(500).send()
     // })
 })
+app.patch('/users/:id',async(req,res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try{
+        const user=await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
+
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+
+    }catch(e){
+        res.status(400).send()
+
+    }
+})
+
+
+app.delete('/users/:id',async (req,res)=>{
+    try{
+        const user=await User.findByIdAndDelete(req.params.id)
+        if(!user){
+            return res.status(404).send()
+
+        }
+        res.send(user)
+    }catch(e){
+
+        res.status(500).send()
+    }
+})
 
 app.listen(port,()=>{
 
