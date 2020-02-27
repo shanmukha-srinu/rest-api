1906308341e9a7451d6ee6c9ac5018b513039a82
shanmukha srinivas <shanmukasrinivas010@gmail.com>
2020-02-27 20:21:44 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -6,36 +6,52 @@ const app=express()
 const port=process.env.PORT ||3000
 
 app.use(express.json())
-app.post('/users',(req,res)=>{
+app.post('/users',async (req,res)=>{
     const user=new User(req.body)
-    user.save().then(()=>{
-        res.send(user)
-    }).catch((e)=>{
-        res.status(500).send()
+    try{
+        await user.save()
+        res.status(201).send(user)
+    }catch(e){
+        res.status(404).send(e)
+    }
+    // user.save().then(()=>{
+    //     res.send(user)
+    // }).catch((e)=>{
+    //     res.status(500).send()
         
 
-    })
+    // })
 })
-app.get('/users',(req,res)=>{
-
-    User.find({}).then((user)=>{
-        res.send(user)
-    }).catch((e)=>{
-
-    })
-
+app.get('/users',async (req,res)=>{
+    try{
+        const users= await User.find({})
+        res.send(users)
+    }
+    catch(e){
+        res.status(500).send()
+    }
 })
 
-app.get('/users/:id',(req,res)=>{
+app.get('/users/:id',async (req,res)=>{
     const _id=req.params.id
-    User.findById(_id).then((user)=>{
+    try{
+        const  user=await User.findById(_id)
         if(!user){
             return res.status(404).send()
         }
         res.send(user)
-    }).catch(()=>{
+    }catch(e){
         res.status(500).send()
-    })
+
+    }
+    // User.findById(_id).then((user)=>{
+    //     if(!user){
+    //         return res.status(404).send()
+    //     }
+    //     res.send(user)
+    // }).catch(()=>{
+    //     res.status(500).send()
+    // })
 })
 
 
